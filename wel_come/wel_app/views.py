6a781ec7e2627b3acebaf99160f2eae139906830
kavitha <kavinu2010@gmail.com>
2024-12-09 21:59:38 +0100

    models_welcome_added

--- wel_come/wel_app/views.py
+++ wel_come/wel_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from . import views
+from django.http import HttpResponse
+from .models import Comment
 
 # Create your views here.
+def home(request):
+  return HttpResponse('welcome home')
+
+def comments(request):
+  comm=Comment.object.all()
+  return render(request,'comment.html',{'comm':comm})
